b3d41aedb7124bd287a2799f8464cd70faa836f4
Cinob <cinob@foxmail.com>
2021-10-25 16:05:36 +0800

    Update shims.d.ts

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -5,8 +5,8 @@ declare interface Window {
 }
 
 // with vite-plugin-md, markdowns can be treat as Vue components
-declare module '*.md' {
-  import { ComponentOptions } from 'vue'
-  const component: ComponentOptions
+declare module '*.vue' {
+  import { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
   export default component
 }
